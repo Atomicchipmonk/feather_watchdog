f61ec6f44ceb83f98b4b355d929991c1b484554d
atomicchipmonk <spaceterps@gmail.com>
2018-12-09 00:54:09 +0000

    all configuration is now in the docker-compose.yml

--- get_onboard_readings.py
+++ get_onboard_readings.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import os
 
 def read_input(current_name, decimator, unit):
   current_fd = open('/sys/devices/3160000.i2c/i2c-0/0-0041/iio_device/'+current_name)
@@ -25,13 +26,16 @@ power_input_list=[
 "in_power1_input",
 "in_power2_input"]
 
+influx_url = os.environ['INFLUXDB_URL']
+unit_id = os.environ['UNIT_ID']
+
 
 while True:
   for input in current_input_list:
     line = read_input(input, 10, 'mAmps')
     print line
     try:
-      resp = requests.post('http://192.168.1.110:8086/write?db=tx2i_1', data=line)
+      resp = requests.post(influx_url+'/write?db='+unit_id, data=line)
       print resp
     except:
      print "Unable to connect"
@@ -39,7 +43,7 @@ while True:
     line = read_input(input, 1000, 'Volts')
     print line
     try:
-      resp = requests.post('http://192.168.1.110:8086/write?db=tx2i_1', data=line)
+      resp = requests.post(influx_url+'/write?db='+unit_id, data=line)
       print resp
     except:
      print "Unable to connect"
@@ -47,7 +51,7 @@ while True:
     line = read_input(input, 10000, 'Watts')
     print line
     try:
-      resp = requests.post('http://192.168.1.110:8086/write?db=tx2i_1', data=line)
+      resp = requests.post(influx_url+'/write?db='+unit_id, data=line)
       print resp
     except:
      print "Unable to connect"
